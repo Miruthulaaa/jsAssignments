651f863fe89b3fa728222e725180cee511812a85
Miruthulaaa <36847414+Miruthulaaa@users.noreply.github.com>
2020-07-20 13:14:38 +0530

    Add files via upload

---
+++ day4/assignment3/assignment3.js
@@ -0,0 +1,5 @@
+let shoppinglist = ['oil', 'pencil', 'books', 'can', 'bucket'];
+// let shoppingBucket = shoppinglist.slice(); 
+// let shoppingBucket = [...shoppinglist]; /*or*/  shoppingBucket = [...shoppinglist, 'Doctor Set'];
+shoppingBucket.push('mug', 'vessels', 'bin');
+console.log(`old list : ${shoppinglist} and new list : ${shoppingBucket}` )
